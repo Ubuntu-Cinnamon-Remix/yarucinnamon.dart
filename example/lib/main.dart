023695d09332928313e51abd041317e4e9283e8a
Joshua Peisach <itzswirlz2020@outlook.com>
2021-02-16 14:50:08 -0500

    Example: Deprecation fixes
    FlatButton -> TextButton
    RaisedButton -> ElevatedButton
    
    Checkbox, Switch, Radio - add some onchanged fields

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -127,13 +127,15 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.overline,
             ),
             Row(children: <Widget>[
-              FlatButton(
-                onPressed: () => print('FlatButton'),
+              TextButton(
+                onPressed: () => print('TextButton'),
                 child: const Text('Click me!'),
               ),
               const SizedBox(width: 15),
-              FlatButton(
+              TextButton(
+                onPressed: null,
                 child: const Text("Can't click me!"),
+                autofocus: true,
               ),
             ]),
             Row(children: <Widget>[
@@ -148,12 +150,13 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ]),
             Row(children: <Widget>[
-              RaisedButton(
-                onPressed: () => print('RaisedButton'),
+              ElevatedButton(
+                onPressed: () => print('ElevatedButton'),
                 child: const Text('Click me!'),
               ),
               const SizedBox(width: 15),
-              RaisedButton(
+              ElevatedButton(
+                onPressed: null,
                 child: const Text("Can't click me!"),
               ),
             ]),
@@ -189,7 +192,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     Row(
                       children: <Widget>[
-                        Checkbox(value: true),
+                        Checkbox(
+                          value: true,
+                          onChanged: null,
+                        ),
                         const Text('Disabled'),
                       ],
                     ),
@@ -206,7 +212,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     Row(
                       children: <Widget>[
-                        Switch(onChanged: (value) {}, value: false),
+                        Switch(
+                          value: true,
+                          onChanged: (bool value) {},
+                        ),
                         const Text('No'),
                       ],
                     ),
@@ -235,7 +244,11 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     Row(
                       children: <Widget>[
-                        Radio(value: 3, groupValue: 1),
+                        Radio(
+                          value: 3,
+                          groupValue: 1,
+                          onChanged: (int value) {},
+                        ),
                         const Text('Disabled'),
                       ],
                     ),
